9655b44d8a0c3bc83dc16b949349c12b6850568e
J. Maynard-Zhang <71403391+jmaynard-zhang@users.noreply.github.com>
2022-01-18 14:21:25 -0800

    Clarify list question

--- Exercise-8-1.R
+++ Exercise-8-1.R
@@ -6,8 +6,9 @@ my_breakfast <- c("toast", "eggs", "tea")
 # Create a vector `my_lunch` of everything you ate (or will eat) for lunch
 my_lunch <- c("soup", "pb + j")
 
-# Create a list `meals` that has contains your breakfast and lunch
-meals <- list(breakfast = my_breakfast, lunch = my_lunch)
+# Create a list `meals` that has contains your breakfast and lunch (remember to
+# label your list elements using `=`)
+meals <- list(my_breakfast_element = my_breakfast, my_lunch_element = my_lunch)
 
 # Add a "dinner" element to your `meals` list that has what you plan to eat
 # for dinner
@@ -19,7 +20,7 @@ dinner <- meals$dinner
 
 # Use double-bracket notation to extract your `my_lunch` element from your list
 # and save it in your list as the element at index 5 (no reason beyond practice)
-meals[[5]] <- meals[["my_lunch"]]
+meals[[5]] <- meals[["my_lunch_element"]]
 
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early_meals`
