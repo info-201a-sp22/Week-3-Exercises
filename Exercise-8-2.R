3ecd654ff7da50c1efdfa1d1d419b1f38e5caff7
melaniewalsh <melanie.walsh@cornell.edu>
2022-01-18 02:00:49 -0800

    add solutions

---
+++ Exercise-8-2.R
@@ -0,0 +1,37 @@
+# Exercise 8-2
+
+# Create a *list* of 10 random numbers by using the `runif()` function (e.g., runif(10)), then use `as.list()` to convert it to a list
+# Save this list as a variable called `numbers`
+numbers <- as.list(runif(10))
+
+# Use `lapply()` to apply the `round()` function to each number in the list of numbers. Try rounding by 1 digit and then 2 digits
+lapply(numbers, round, digits=2)
+
+# Assign the opening lines of Jane Austen's Pride and Prejudice to the variable `austen_text`
+austen_text <- "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife. However little known the feelings or views of such a man may be on his first entering a neighbourhood, this truth is so well fixed in the minds of the surrounding families, that he is considered as the rightful property of some one or other of their daughters."
+
+# Use the `strsplit()` function to split this text into a vector of words. Save this list as the variable `all_words`
+# Hint: split on `" "` to split on white space or "\\W+" to split on white space and punctuation
+# Note: this will return a _list_ with 1 element (which is the vector of letters)
+
+all_words <- strsplit(tolower(austen_text), "\\W+")
+
+# Extract the vector of words from the resulting list, and assign it to the variable `words`
+words <- all_words[[1]]
+
+# Use the `unique()` function to get a vector of unique words
+unique(words)
+
+# Define a function `count_occurrences` that takes in two parameters: a words and a vector of words. The function should return how many times that letter
+# occurs in the provided vector.
+# Hint: use a filter operation!
+
+count_occurrences <- function(word, all_words) {
+  length(all_words[all_words == word])
+}
+
+# Call your `count_occurrences()` function to see how many times the following words appear in the text
+
+count_occurrences("man", words)
+count_occurrences("truth", words)
+count_occurrences("is", words)
