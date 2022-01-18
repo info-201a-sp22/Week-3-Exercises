9c95cc53a66264c536896ada7cc017a800e238e8
melaniewalsh <melanie.walsh@cornell.edu>
2022-01-18 02:04:42 -0800

    add exercises

---
+++ Hiring-Algorithms.R
@@ -0,0 +1,68 @@
+# Hiring Algorithms
+
+# Create a program to evaluate whether job applicants should move on in the job search process based on their grades
+
+# Student 1
+# Grades: Class 1 — 3.6, Class 2 — 3.6, Overall GPA — 3.6
+
+# Student 2
+# Grades: Class 1 — 2.9, Class 2 — 4.0, Overall GPA — 3.5
+
+# Student 3
+# Grades: Class 1 — 3.9, Class 2 — 3.1, Overall GPA — 3.4
+
+
+# Applicants should only be accepted if their class average is
+# over 3.5 and overall GPA is over 3.5
+
+analyze_applicant1 <-
+}
+
+# Test your function
+# Should be accepted
+analyze_applicant1(3.6, 3.6, 3.6)
+# Should be rejected
+analyze_applicant1(2.9, 4.0, 3.5)
+
+# Applicant should be rejected if any of their class grades or
+# their overall GPA is lower than 3.0
+analyze_applicant2 <- 
+
+# Test your function
+# Should be accepted
+analyze_applicant2(3.6, 3.6, 3.6)
+# Should be rejected
+analyze_applicant2(2.9, 4.0, 3.5)
+
+# Applicant should be accepted if they
+# earned a 4.0 in any class or overall GPA
+
+analyze_applicant3 <-
+
+# Test your function
+# Should be rejected
+analyze_applicant3(3.6, 3.6, 3.6)
+# Should be accepted
+analyze_applicant3(2.9, 4.0, 3.5)
+
+# Your Turn!
+
+# Now talk with your group and come up with your own custom function for evaluating candidates
+# What do you think is the most fair and/or most efficient way of judging applicants?
+# You can also add in an "else if" statement that provides an option other than accept or reject
+
+revised_analyze_applicant <-
+
+
+
+
+
+
+
+# Test your function
+# Student 1
+analyze_applicant3(3.6, 3.6, 3.6)
+# Student 2
+analyze_applicant3(2.9, 4.0, 3.5)
+# Student 3
+analyze_applicant3(3.9, 3.1, 3.4)
